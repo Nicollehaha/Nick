ad84711e29efcd1c994a95b7fbb01774d2372d41
Nicollehaha <paes.nicolle@escola.pr.gov.br>
2025-06-27 00:01:17 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -15,4 +15,18 @@ criaCartao(
     'Qual a musica mais famosa da Rita Lee?',
     'Mania de Vocễ'
 )
-
+criaCartao(
+    'Pergunta',
+    'com quantos anos Chico Buarque começou no mundo da musica?',
+    '5 anos'
+)
+criaCartao(
+    'Pergunta',
+    'qual a musica mais famosa do Chico Buarque?',
+    'Construção'
+)
+criaCartao(
+    'Pergunta',
+    'Com quantos anos Rita Lee começou no mundo da musica?',
+    '15 anos'
+)
